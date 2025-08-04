57c9388f85b6569767f8d8e74ea1f5c8a47b247b
Marcella Aleo <cella.aleo@gmail.com>
2025-08-04 19:53:49 +0100

    1.1 - course-information - Modifies main.jsx and creates new components in App.jsx.

--- part-1/course-information/src/App.jsx
+++ part-1/course-information/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{props.course}</h1>
     </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.count}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 700 },
+    { part: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={parts[0].part} exercises={parts[0].exercises} />
+      <Content part={parts[1].part} exercises={parts[1].exercises} />
+      <Content part={parts[2].part} exercises={parts[2].exercises} />
+      <Total count={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+    </div>
   )
 }
 
