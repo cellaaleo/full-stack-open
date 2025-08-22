8ca75397fd532e1df43153300acd4ca90511b9f5
Marcella Aleo <cella.aleo@gmail.com>
2025-08-22 17:46:25 +0100

    1.11 - unicafe - Display the statistics in a table.(and some refactoring)

--- part-1/unicafe/README.md
+++ part-1/unicafe/README.md
@@ -1,5 +1,5 @@
 # Unicafe
 
-The task is to implement a web application for collecting customer feedback for the University of Helsinki Unicafe, with only three options: good, neutral, and bad, and then display the total number of feedback for each category.
+The task was to implement a web application for collecting customer feedback for the University of Helsinki Unicafe, with only three options: good, neutral, and bad. Once the first feedback is given, the application displays some statistics data.
 
 The app works during a single browser session. Once you refresh the page, the collected feedback disappears.
--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from 'react'
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <th>{text}</th>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad
-  const average = ((props.good * 1) + (props.bad * -1)) / all
-  const positive = (props.good / all) * 100
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = ((good * 1) + (bad * -1)) / all
+  const positive = ((good / all) * 100) + " %"
 
   if (all === 0) {
     return <p>No feedback given</p>
   }
 
   return (
-    <>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   )
 }
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
