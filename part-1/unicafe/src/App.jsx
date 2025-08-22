336d96f2a55d9c9c6c969837978207e637b38a1c
Marcella Aleo <cella.aleo@gmail.com>
2025-08-22 16:38:42 +0100

    1.10 - unicafe - Create new components: Button and StatisticLine. (and refactor Statistics)

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,31 +1,29 @@
 import { useState } from 'react'
 
+const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+
 const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad
   const average = ((props.good * 1) + (props.bad * -1)) / all
   const positive = (props.good / all) * 100
 
   if (all === 0) {
-    return (
-      <>
-        <h1>Statistics</h1>
-        <p>No feedback given</p>
-      </>
-    )
+    return <p>No feedback given</p>
   }
 
   return (
     <>
-      <h1>Statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </>
   )
 }
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -48,10 +46,11 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
+      <Button text="good" onClick={handleGoodClick} />
+      <Button text="neutral" onClick={handleNeutralClick} />
+      <Button text="bad" onClick={handleBadClick} />
 
+      <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
