f48473e0316134d864751757c8385bae527551af
Marcella Aleo <cella.aleo@gmail.com>
2025-09-12 14:20:23 +0100

    2.5 - course-information - Course component is now a separate module imported by the App component.

--- part-2/course-information/src/App.jsx
+++ part-2/course-information/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => 
-      <Part key={part.id} part={part} />
-    )}
-  </div>
-)
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return <strong>total of {total} exercises</strong>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
