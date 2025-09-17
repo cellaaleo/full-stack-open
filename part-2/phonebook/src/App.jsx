e4cf109dccdc5870108c3d3415316f52ef8a66d1
Marcella Aleo <cella.aleo@gmail.com>
2025-09-17 20:01:18 +0100

    2.6 - phonebook - the app shows a list of names. It is possible to add new names using the form.

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => <li>{person.name}</li>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
-  console.log('aqui')
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const personObject = { name: newName }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+            <Person key={person.name} person={person} />
+        )}
+      </ul>
     </div>
   )
 }
