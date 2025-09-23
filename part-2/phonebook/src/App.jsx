eb21d2fa134040c71b19e36600341fdafff00a7a
Marcella Aleo <cella.aleo@gmail.com>
2025-09-23 17:27:47 +0100

    2.9 - phonebook - users can filter the list of people by name.

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -4,13 +4,22 @@ const Person = ({ person }) => <li>{person.name} {person.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value.replace(/[^0-9-]/, ''))
+  const handleSearchChange = (event) => setNewSearch(event.target.value)
+
+  const result = persons.filter((person) => 
+    person.name.toLowerCase().includes(newSearch.toLowerCase())
+  )
 
   const addName = (event) => {
     event.preventDefault()
@@ -28,6 +37,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          Filter by name <input type="search" value={newSearch} onChange={handleSearchChange} />
+        </div>
+
+      <h2>Add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -39,13 +53,14 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <div>debug: {newNumber}</div>
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {result.map(person => 
             <Person key={person.name} person={person} />
         )}
       </ul>
+
     </div>
   )
 }
